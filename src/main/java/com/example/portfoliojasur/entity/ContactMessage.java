bd4cb1ddfc505f2c31871efb93617a125b485b23
Jasurrasul <rasulovjasur995@gmail.com>
2025-10-07 16:18:56 +0200

    Skapade typ(dto)

---
+++ src/main/java/com/example/portfoliojasur/entity/ContactMessage.java
@@ -0,0 +1,38 @@
+package com.example.portfoliojasur.entity;
+
+
+
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "contact_messages")
+public class ContactMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, length=100)
+    private String name;
+
+    @Column(nullable=false, length=200)
+    private String email;
+
+    @Column(nullable=false, length=2000)
+    private String message;
+
+    @Column(nullable=false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+    public OffsetDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(OffsetDateTime createdAt) { this.createdAt = createdAt; }
+}
