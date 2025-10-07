7309b1b1e17cf5933312dd4537c855a69dc710e0
Jasurrasul <rasulovjasur995@gmail.com>
2025-10-07 17:35:00 +0200

    Skapade repo

---
+++ src/main/java/com/example/portfoliojasur/repo/ContactMessageRepository.java
@@ -0,0 +1,7 @@
+package com.example.portfoliojasur.repo;
+
+import com.example.portfoliojasur.entity.ContactMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {}
+
