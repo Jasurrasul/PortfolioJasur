9849d33bbdf450f9ba4d57d7cf6dfe54a9260351
Jasurrasul <rasulovjasur995@gmail.com>
2025-10-10 18:18:55 +0200

    security config andra

--- src/main/java/com/example/portfoliojasur/controller/IndexController.java
+++ src/main/java/com/example/portfoliojasur/controller/IndexController.java
@@ -1,22 +1,25 @@
 package com.example.portfoliojasur.controller;
 
-
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/index", "/index.html"})
-    public ResponseEntity<Resource> index() {
-        Resource resource = new ClassPathResource("static/index.html");
-        return ResponseEntity
-                .ok()
+    public ResponseEntity<Resource> index() throws IOException {
+        Resource resource = new ClassPathResource("/static/index.html");
+        if (!resource.exists()) {
+            // fallback: kanske ligger direkt på classpath utan /static/
+            resource = new ClassPathResource("index.html");
+        }
+
+        return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_HTML)
                 .body(resource);
     }
